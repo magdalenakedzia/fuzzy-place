771957fcd0b5aea58a0233b995f75d8c0f2a3da6
Magda <magdalena.kedzia3@gmail.com>
2022-11-19 00:18:56 +0100

    replace header with sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
-import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { Home } from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Sidebar />
       <Home />
       
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,37 @@
+import 'material-icons/iconfont/material-icons.css';
+
+import "./style.less";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-           
+            <div className="layout__sidebar">
+                <a className="sidebar__trigger" href="#0">
+                    <span className="material-icons menu">
+                        menu
+                    </span>
+                </a>
+
+                <nav className="sidebar__nav">
+                    <ul>
+                        <li>
+                            <a className="sidebar__nav-link" href="#0">
+                                <i className="fa fa-home"></i><em>Home</em>
+                            </a>
+                        </li>
+                        <li>
+                            <a className="sidebar__nav-link" href="#0">
+                                <i className="fa fa-user"></i><em>My Profile</em>
+                            </a>
+                        </li>
+                        <li>
+                            <a className="sidebar__nav-link" href="#0">
+                                <i className="fa fa-camera"></i><em>Camera</em>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 }
