16f476d1a49f784135769b39c6ae426e4bb88923
Magda <magdalena.kedzia3@gmail.com>
2022-11-19 01:13:53 +0100

    sidebar update

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,35 +1,23 @@
 import 'material-icons/iconfont/material-icons.css';
-
+import { Avatar } from './SidebarElements/Avatar';
+import List from "./SidebarElements/List"
 import "./style.less";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="layout__sidebar">
-                <a className="sidebar__trigger" href="#0">
+            <div className="layout-sidebar">
+                <a className="sidebar-trigger" href="#0">
                     <span className="material-icons menu">
                         menu
                     </span>
                 </a>
 
-                <nav className="sidebar__nav">
-                    <ul>
-                        <li>
-                            <a className="sidebar__nav-link" href="#0">
-                                <i className="fa fa-home"></i><em>Home</em>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="sidebar__nav-link" href="#0">
-                                <i className="fa fa-user"></i><em>My Profile</em>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="sidebar__nav-link" href="#0">
-                                <i className="fa fa-camera"></i><em>Camera</em>
-                            </a>
-                        </li>
-                    </ul>
+                <nav className="sidebar-nav">
+                    {/* <div className=> */}
+                    <Avatar />
+                    <List />
+                    {/* </div> */}
                 </nav>
             </div>
         </div>
---
+++ src/components/Sidebar/SidebarElements/List.tsx
@@ -0,0 +1,24 @@
+function List() {
+    return (
+        <ul>
+            <li>
+                <a className="sidebar-nav-link" href="#0">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a className="sidebar-nav-link" href="#0">
+                    <span>My Profile</span>
+                </a>
+            </li>
+            <li>
+                <a className="sidebar-nav-link" href="#0">
+                    <span>Camera</span>
+                </a>
+            </li>
+        </ul>
+
+    );
+}
+
+export default List;
